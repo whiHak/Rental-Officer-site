f7242c15e8a2df9150c77c6ef3d426bc34569c05
whiHak <betse1111111111@gmail.com>
2025-03-19 01:08:26 +0300

    FIX: merge confilct

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,6 +1,5 @@
 import { model, models, Schema } from "mongoose";
 
-<<<<<<< HEAD
 export interface IUser extends Document {
     _id: string;
     clerkId: string
@@ -22,15 +21,3 @@ const UserSchema = new Schema({
 
 const User = models.User || model("User", UserSchema);
 export default User
-=======
-const UserSchema = new Schema({
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  // Add other user fields as needed
-}, {
-  timestamps: true
-});
-
-const User = models.User || model("User", UserSchema);
-export default User; 
->>>>>>> 0882c53cbf2467dc66111bcba5573c2a9f569bd4
